b3d6dc7576e93b2a49826d01b41c0a683698daac
Roman <novikovroman123@mail.ru>
2023-11-05 00:38:57 +0300

    footer fixed

--- apps/client/src/components/footer/Footer.jsx
+++ apps/client/src/components/footer/Footer.jsx
@@ -1,20 +1,22 @@
-import logo from '../../assets/icons/easy-workshop-logo-black.svg';
+import logo from "../../assets/icons/easy-workshop-logo-black.svg";
 
-import * as style from './Footer.module.scss';
+import * as style from "./Footer.module.scss";
 
 const Footer = () => {
-    return (
-        <div className={style.wrapper}>
-            <div className={style.container}>
-                <div className=''>
-                    <a href='/'>
-                        <img className={style.logo} src={logo} alt='easy workshop' />
-                    </a>
-                </div>
-                <div className={style.copyright}>Copyright © 2023 Matsvei Shulman & Roman Novikov | All Rights Reserved </div>
-            </div>
+  return (
+    <div className={style.wrapper}>
+      <div className={style.container}>
+        <div className="">
+          <a href="/">
+            <img className={style.logo} src={logo} alt="easy workshop" />
+          </a>
         </div>
-    )
-}
+        <div className={style.copyright}>
+          Copyright © 2023 Matsvei Shulman & Roman Novikov | All Rights Reserved{" "}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
--- apps/client/src/components/navBar/NavBar.jsx
+++ apps/client/src/components/navBar/NavBar.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
-import logo from '../../assets/icons/easy-workshop-logo-black.svg';
-import search_icon from '../../assets/icons/search-icon.svg';
+import { useState } from "react";
+import logo from "../../assets/icons/easy-workshop-logo-black.svg";
+import search_icon from "../../assets/icons/search-icon.svg";
 
-import * as style from './Navbar.module.scss';
+import * as style from "./Navbar.module.scss";
 
 const Navbar = (props) => {
   const [auth, setAuth] = useState(false);
@@ -10,45 +10,45 @@ const Navbar = (props) => {
   return (
     <div className={style.wrapper}>
       <div className={style.container}>
-        <div className=''>
-          <a href='/'>
-            <img className={style.logo} src={logo} alt='easy workshop' />
+        <div className="">
+          <a href="/">
+            <img className={style.logo} src={logo} alt="easy workshop" />
           </a>
         </div>
         <nav className={style.nav}>
           <ul className={style.menu}>
             <li className={style.menu_item}>
-              <a href='/'>Home</a>
+              <a href="/">Home</a>
             </li>
             <li className={style.menu_item}>
-              <a href='#'>About</a>
+              <a href="#">About</a>
             </li>
             <li className={style.menu_item}>
-              <a href='#'>Contact</a>
+              <a href="#">Contact</a>
             </li>
           </ul>
-          <form className={style.search} method='' action=''>
+          <form className={style.search} method="" action="">
             <input
               className={style.search_input}
-              type='text'
-              placeholder='Search for...'
+              type="text"
+              placeholder="Search for..."
             />
-            <button className={style.search_submit} type='submit'>
-            <img className='' src={search_icon} alt='search-icon' />
+            <button className={style.search_submit} type="submit">
+              <img className="" src={search_icon} alt="search-icon" />
             </button>
           </form>
           <div className={style.account}>
             <button
-                className={style.account_btn}
-                style={{ display: auth ? 'none' : 'block' }}
+              className={style.account_btn}
+              style={{ display: auth ? "none" : "block" }}
             >
-                Get started
+              Get started
             </button>
             <button
-                className={style.account_btn}
-                style={{ display: auth ? 'block' : 'none' }}
+              className={style.account_btn}
+              style={{ display: auth ? "block" : "none" }}
             >
-                Profile
+              Profile
             </button>
           </div>
         </nav>
