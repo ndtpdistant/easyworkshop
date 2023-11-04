e85627a152b43ea3b234f870ab5f7eac4da0cbfc
Roman <novikovroman123@mail.ru>
2023-11-04 10:59:03 +0300

    client-init

--- apps/client/src/App.jsx
+++ apps/client/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './assets/scss/app/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
