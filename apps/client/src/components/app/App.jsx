482a2798ed6fc08ba40e879ab150f0220103b906
Roman <novikovroman123@mail.ru>
2023-11-04 16:29:36 +0300

    client-update

---
+++ apps/client/src/components/app/App.jsx
@@ -0,0 +1,13 @@
+import './app.scss'
+import NavBar from '../navBar/NavBar'
+
+function App() {
+
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  )
+}
+
+export default App
--- apps/client/src/main.jsx
+++ apps/client/src/main.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './components/app/App'
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+
+root.render(<App/>)
