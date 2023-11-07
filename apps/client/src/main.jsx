f36cae3754de462ae1e6ee210631aea13c781268
h13q17 <matvejsh17@gmail.com>
2023-11-07 13:24:32 +0300

    router init

--- apps/client/src/main.jsx
+++ apps/client/src/main.jsx
@@ -2,11 +2,22 @@ import App from './components/app/App';
 import { createRoot } from 'react-dom/client';
 import './assets/styles/font-import.scss';
 import './assets/styles/global-reset.scss';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
 
+const router = createBrowserRouter([
+  // template
+  // https://reactrouter.com/en/main/start/tutorial
+  {
+    path: '/',
+    element: '',
+    errorElement: <ErrorPage />,
+  }
+])
+
 root.render(
   <>
-    <App />
+    <RouterProvider router={router} />
   </>,
 );
