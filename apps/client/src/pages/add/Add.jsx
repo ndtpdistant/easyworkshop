54f9296c9f0f67b3003c26698bde2a0b8f0ce97f
Roman <novikovroman123@mail.ru>
2023-11-16 10:27:36 +0300

    add page

--- apps/client/src/pages/add/Add.jsx
+++ apps/client/src/pages/add/Add.jsx
@@ -1,12 +1,15 @@
-import { useNavigate } from 'react-router-dom';
+import { Form, useNavigate } from 'react-router-dom';
 
 import Button from '../../components/Button';
 import FileDropzone from '../../components/FileDropzone';
 
 import style from './Add.module.scss';
 import { useState } from 'react';
+import Input from '../../components/Input';
+import useInput from '../../hooks/useInput';
 
 const Add = () => {
+  const title = useInput('', { isEmpty: true, maxLengthError: 254 });
   const [filesLength, setFilesLength] = useState(0);
   const [error, setError] = useState(null);
   const [step, setStep] = useState(1);
@@ -77,8 +80,34 @@ const Add = () => {
               initialError={error}
               step={2}
             />
+            <Form>
+              <div className={style.inputWrapper}>
+                <label>Title</label>
+                <Input
+                  name={'title'}
+                  onChange={(e) => {
+                    title.onChange(e);
+                  }}
+                  onBlur={(e) => title.onBlur(e)}
+                  value={title.value}
+                  type={'text'}
+                  inlineStyle={{
+                    borderRadius: '5px',
+                    border: '1px solid #979797',
+                    background: '#FFF',
+                  }}
+                />
+              </div>
+              <div className={style.inputWrapper}>
+                <label>Description</label>
+                <textarea maxLength={1024} />
+              </div>
+            </Form>
             <Button
               onClick={() => {
+                if (!title.inputValid) {
+                  setError('You must to write the title');
+                }
                 if (filesLength === 0) {
                   setError('You must upload at least one image');
                 } else {
@@ -86,7 +115,11 @@ const Add = () => {
                   setStep((prevStep) => prevStep + 1);
                 }
               }}
-              inlineStyle={{ width: '345px', height: '45px', marginTop: '20px', }}
+              inlineStyle={{
+                width: '345px',
+                height: '45px',
+                marginTop: '20px',
+              }}
             >
               Publish
             </Button>
